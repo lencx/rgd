cd24b2dc22b92b70235ad0cf0f911c28e2572a16
lencx <cxin1314@gmail.com>
2021-06-16 14:28:05 +0800

    chore: write xml

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # rgd
+
+> RSS - GitHub Discussions API
+
+```bash
+npm install -g rgd
+
+# or
+npm install -D rgd
+```
+
+```bash
+Usage: rgd
+
+Options:
+  --owner
+  --repo
+  --token: generate token -> https://github.com/settings/tokens/new
+  --limit: if not set, all are requested by default, value is number, no more than 100.
+  --outdir: default `.`
+  --filename: default `feed.xml`
+  --site-title: default `RSS`
+  --site-link: defalut `/`
+  --site-desc: defalut `GitHub Discussions`
+```
--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const dayjs = require('dayjs');
 const { graphql } = require("@octokit/graphql");
 const argv = require('minimist')(process.argv.slice(2));
 
-// const cwd = process.cwd();
-
-const { owner, repo, token, } = argv;
+const { owner, repo, token } = argv;
 
 const graphqlClient = graphql.defaults({
   headers: {
@@ -28,50 +26,81 @@ async function init() {
     process.exit();
   }
 
-  const repoLink = `https://github.com/${owner}/${repo}`;
-
-  const { repository } = await graphqlClient(`
-    query ($owner: String!, $repo: String!) {
-      repository(owner: $owner, name: $repo) {
-        discussions(first: 3) {
-          edges {
-            node {
-              title
-              number
-              updatedAt
-              bodyHTML
-              category {
-                name
-                # emojiHTML
+  const fetchData = async (size, lastCursor) => {
+    const { repository } = await graphqlClient(`
+      query ($owner: String!, $repo: String!, $limit: Int!, $cursor: String) {
+        repository(owner: $owner, name: $repo) {
+          discussions(first: $limit, after: $cursor) {
+            edges {
+              cursor
+              node {
+                title
+                number
+                updatedAt
+                bodyHTML
               }
             }
           }
         }
       }
-    }
-  `, {
-    owner,
-    repo,
-  });
-  // console.log(JSON.stringify(repository, null, 2));
-  const { discussions } = repository;
-  if (discussions) {
-    let content = '';
-    discussions.edges.forEach(({ node }) => {
-      const issuesLink =  `${repoLink}/discussions/${node.number}`;
-      content += postItem({ title: node.title, link: issuesLink, date: dayjs(node.updatedAt).format('YYYY-MM-DD'), html: node.bodyHTML });
+    `, {
+      owner,
+      repo,
+      limit: size,
+      cursor: lastCursor,
+    });
+    return repository.discussions.edges;
+  }
+
+  const repoLink = `https://github.com/${owner}/${repo}`;
+  let totalCount;
+  let limit = 20;
+  let list = [];
+  let last = null;
+
+  if (!argv.limit) {
+    const _data = await graphqlClient(`
+      query ($owner: String!, $repo: String!) {
+        repository(owner: $owner, name: $repo) {
+          discussions {
+            totalCount
+          }
+        }
+      }
+    `, {
+      owner,
+      repo,
     });
 
-    const feedxml = feedXML({
-      siteTitle: '浮之静',
-      siteLink: 'https://z.nofwl.com',
-      siteDesc: '浮之静 技术社区',
-      feed: 'feed.xml',
-      postItems: content,
-    })
-
-    fs.writeFileSync(path.resolve('.', 'feed.xml'), feedxml);
+    totalCount = _data.repository.discussions.totalCount;
+    for (let i = 0; i < Math.ceil(totalCount / limit); i++) {
+      const temp = await fetchData(limit, last);
+      last = Array.from(temp).pop().cursor;
+      list = [...list, ...temp];
+    }
+  } else {
+    limit = +argv.limit;
+    const temp = await fetchData(limit, null);
+    list = temp;
   }
+
+
+  let content = '';
+  list.forEach(({ node }) => {
+    const issuesLink =  `${repoLink}/discussions/${node.number}`;
+    content += postItem({ title: node.title, link: issuesLink, date: dayjs(node.updatedAt).format('YYYY-MM-DD'), html: node.bodyHTML });
+  });
+
+  const feedxml = feedXML({
+    siteTitle: argv['site-title'] || 'RSS',
+    siteLink: argv['site-link'] || '/',
+    siteDesc: argv['site-desc'] || 'GitHub Discussions',
+    filename: argv.filename || 'feed.xml',
+    postItems: content,
+  })
+
+  if (argv.outdir) fs.mkdirSync(path.resolve(argv.outdir), { recursive: true })
+  fs.writeFileSync(path.resolve(argv.outdir || '.', 'feed.xml'), feedxml);
 }
 
 init().catch((e) => {
@@ -80,8 +109,18 @@ init().catch((e) => {
 
 function cmdHelp() {
   return console.log(`
-usage: rgd [--owner] [--repo] [--token] [--outdir]
-generate token: ${chalk.blue('https://github.com/settings/tokens/new')}`);
+usage: rgd
+
+options:
+  --owner
+  --repo
+  --token: generate token -> https://github.com/settings/tokens/new
+  --limit: if not set, all are requested by default, value is number, no more than 100.
+  --outdir: default \`.\`
+  --filename: default \`feed.xml\`
+  --site-title: default \`RSS\`
+  --site-link: defalut \`/\`
+  --site-desc: defalut \`GitHub Discussions\``);
 }
 
 function postItem({ title, link, date, html }) {
@@ -95,11 +134,11 @@ function postItem({ title, link, date, html }) {
 `
 }
 
-function feedXML({ siteTitle, siteLink, siteDesc, feed, postItems }) {
+function feedXML({ siteTitle, siteLink, siteDesc, filename, postItems }) {
   return `<?xml version=\"1.0\" encoding=\"utf-8\"?><rss xmlns:atom=\"http://www.w3.org/2005/Atom\" version=\"2.0\">
 <channel>
   <title>${siteTitle}</title>
-  <atom:link href=\"${siteLink}/${feed}\" rel=\"self\" type=\"application/rss+xml\" />
+  <atom:link href=\"${siteLink.replace(/\/$/, '')}/${filename}\" rel=\"self\" type=\"application/rss+xml\" />
   <link>${siteLink}</link>
   <description>${siteDesc}</description>
   ${postItems}
